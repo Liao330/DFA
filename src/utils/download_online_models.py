9bf6660364ecc51c7b18c086e41b3e8ad1f58f3a
Liao330 <ljf0114@gmail.com>
2025-03-11 21:53:33 +0800

    '忽略此处commit'

--- src/utils/download_online_models.py
+++ src/utils/download_online_models.py
@@ -1,22 +1,22 @@
-from transformers import AutoProcessor, CLIPModel, ViTModel, ViTConfig
-from src.utils.proxy import ProxyManager
-
-proxy = ProxyManager()
-
-def get_clip_visual(proxy=proxy, model_name="openai/clip-vit-base-patch16"):
-    proxy.set_proxy()
-    processor = AutoProcessor.from_pretrained(model_name)
-    model = CLIPModel.from_pretrained(model_name)
-    proxy.unset_proxy()
-    return processor, model.vision_model
-
-
-def get_vit_model(proxy=proxy, model_name="google/vit-base-patch16-224-in21k"):
-    proxy.set_proxy()
-    # processor = AutoProcessor.from_pretrained(model_name)
-    configuration = ViTConfig(
-        image_size=224,
-    )
-    model = ViTModel.from_pretrained(model_name, config=configuration)
-    proxy.unset_proxy()
-    return None, model
+# from transformers import AutoProcessor, CLIPModel, ViTModel, ViTConfig
+# from src.utils.proxy import ProxyManager
+#
+# proxy = ProxyManager()
+#
+# def get_clip_visual(proxy=proxy, model_name="openai/clip-vit-base-patch16"):
+#     proxy.set_proxy()
+#     processor = AutoProcessor.from_pretrained(model_name)
+#     model = CLIPModel.from_pretrained(model_name)
+#     proxy.unset_proxy()
+#     return processor, model.vision_model
+#
+#
+# def get_vit_model(proxy=proxy, model_name="google/vit-base-patch16-224-in21k"):
+#     proxy.set_proxy()
+#     # processor = AutoProcessor.from_pretrained(model_name)
+#     configuration = ViTConfig(
+#         image_size=224,
+#     )
+#     model = ViTModel.from_pretrained(model_name, config=configuration)
+#     proxy.unset_proxy()
+#     return None, model

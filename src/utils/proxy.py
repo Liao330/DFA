0ccee476d5b76b50c088e77f14e96078b3efc28b
Liao330 <ljf0114@gmail.com>
2025-03-11 21:56:53 +0800

    '新增功能：创建网络代理函数并用于下载HugginhFace上的在线模型'

--- src/utils/proxy.py
+++ src/utils/proxy.py
@@ -1,19 +1,21 @@
-# import os
-#
-# class ProxyManager():
-#
-#     def __init__(self):
-#         self.http_proxy = 'http://127.0.0.1:7890' # 本地clash默认地址
-#         self.https_proxy = 'http://127.0.0.1:7890'
-#
-#     def set_proxy(self):
-#         """设置代理"""
-#         os.environ['http_proxy'] = self.http_proxy
-#         os.environ['https_proxy'] = self.https_proxy
-#
-#     def unset_proxy(self):
-#         """取消代理"""
-#         if 'http_proxy' in os.environ:
-#             del os.environ['http_proxy']
-#         if 'https_proxy' in os.environ:
-#             del os.environ['https_proxy']
+import os
+
+class ProxyManager():
+    """
+        需要在服务器上手动配置clash等此类代理软件，运行之后将替换为自己的运行url
+    """
+    def __init__(self):
+        self.http_proxy = 'http://127.0.0.1:7890' # 本地clash默认地址
+        self.https_proxy = 'http://127.0.0.1:7890'
+
+    def set_proxy(self):
+        """设置代理"""
+        os.environ['http_proxy'] = self.http_proxy
+        os.environ['https_proxy'] = self.https_proxy
+
+    def unset_proxy(self):
+        """取消代理"""
+        if 'http_proxy' in os.environ:
+            del os.environ['http_proxy']
+        if 'https_proxy' in os.environ:
+            del os.environ['https_proxy']

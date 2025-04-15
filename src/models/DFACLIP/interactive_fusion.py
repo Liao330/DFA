6a788104f4254131b9c7591b64816f44af77a10c
Liao330 <ljf0114@gmail.com>
2025-04-15 09:11:52 +0800

    针对t-sne代码进行了对应修改

--- src/models/DFACLIP/interactive_fusion.py
+++ src/models/DFACLIP/interactive_fusion.py
@@ -10,10 +10,12 @@ class Interactive_fusion_classifier(nn.Module):
         self.transformer = nn.TransformerEncoder(self.transformer_layer, num_layers=2, enable_nested_tensor=False).cuda()
         self.classifier = nn.Linear(1024, 2).cuda()
 
-    def forward(self, clip_fmp, guide_fmp):
+    def forward(self, clip_fmp, guide_fmp, return_features=False):
         if guide_fmp is None:
             fused_seq_pooled = clip_fmp.mean(dim=1)  # [B, 1024]
-            return self.classifier(fused_seq_pooled)
+            logits = self.classifier(fused_seq_pooled)
+            if return_features:
+                return logits, fused_seq_pooled
         else:
             B = guide_fmp.shape[0]
 
@@ -26,5 +28,6 @@ class Interactive_fusion_classifier(nn.Module):
             fused_seq = self.transformer(combined_seq)  # [B, 434, 1024]
             fused_seq_pooled = fused_seq.mean(dim=1)  # [B, 1024]
             logits = self.classifier(fused_seq_pooled)  # [B, 2]
-
+            if return_features:
+                return logits, fused_seq_pooled
             return logits
